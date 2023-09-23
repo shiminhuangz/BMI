c9a7be48c83e52ba371e484cb228ba2ae30b61e6
karsa <karsa@LAPTOP-EOI7AGB0>
2023-09-23 11:21:34 +0800

    加入專案檔案。

---
+++ BMI/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("請輸入身高(CM):：");
+        double height = Convert.ToDouble(Console.ReadLine());
+        
+        Console.Write("請輸入體重(KG)：");
+        double weight = Convert.ToDouble(Console.ReadLine());
+        height = height / 100;
+        double bmi = weight / (height * height);
+
+        Console.WriteLine($"您的BMI為：{bmi:F2}");
+
+       /*
+        if (bmi < 18.5)
+        {
+            Console.WriteLine("過輕。");
+        }
+        else if (bmi < 24.9)
+        {
+            Console.WriteLine("適中。");
+        }
+        else if (bmi < 29.9)
+        {
+            Console.WriteLine("過重。");
+        }
+        else
+        {
+            Console.WriteLine("肥胖。");
+        }*/
+
+       
+        Console.ReadKey();
+    }
+}
